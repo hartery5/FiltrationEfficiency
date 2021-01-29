6c831e7dc56e92bbae350fb46635c24808abc4eb
hartery5 <78227345+hartery5@users.noreply.github.com>
2021-01-29 14:30:35 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -291,17 +291,17 @@ function updateText(){
     text("Results",10,140);
     textStyle(NORMAL);
     text("Total Filtration Efficiency (SMPS):",10,157.5);
-    text(addunit(FTOT1,'%'),275,157.5);
+    text(addunit(FTOT1,'%'),285,157.5);
     text("Total Filtration Efficiency (Photometer):",10,182.5);
-    text(addunit(FTOT2,'%'),275,182.5);  
+    text(addunit(FTOT2,'%'),285,182.5);  
     text("Methodological Bias (SMPS-Photometer):",10,207.5);
-    text(addunit(FTOT1-FTOT2,'%'),275,207.5);
+    text(addunit(FTOT1-FTOT2,'%'),285,207.5);
     text("Pressure Drop:",10,232.5);
-    text(addunit(DP,'Pa'),275,232.5);  
+    text(addunit(DP,'Pa'),285,232.5);  
     text("Most Penetrating Particle Size:",10,257.5);
-    text(addunit(MPPS*1e9,'nm'),275,257.5);
+    text(addunit(MPPS*1e9,'nm'),285,257.5);
     text("Flow Rate:",10,282.5);
-    text(addunit(_U*100.*141.8/16.67,'L min^-1'),275,282.5);
+    text(addunit(_U*100.*141.8/16.67,'L min^-1'),285,282.5);
 
     // Display Test Result
     textStyle(BOLD);
